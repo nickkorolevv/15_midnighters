97af8fa9af9af0755557048b9402e90f80b3f6fa
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-29 20:29:14 +0300

    delete useless function

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,5 +1,5 @@
 import requests
-import datetime
+from datetime import datetime
 import pytz
 
 
@@ -10,46 +10,38 @@ def load_data():
     while page <= number_of_pages:
         devman_response = requests.get(url, params={"page": page})
         page_info = devman_response.json()
-        number_of_pages = int(page_info["number_of_pages"])
+        number_of_pages = page_info["number_of_pages"]
         for record in page_info["records"]:
             yield record
         page += 1
 
 
-def local_time(time_stamp, timezone):
-    local_date_time = datetime.datetime.fromtimestamp(
-        float(time_stamp),
-        tz=pytz.timezone(timezone)
-    )
-    return local_date_time.time()
+def get_local_time(time_stamp, timezone):
+    local_time = datetime.fromtimestamp(time_stamp, tz=pytz.timezone(timezone))
+    return local_time.hour
 
 
-def get_users(dev_users):
+def get_midnighters(dev_users):
     get_users_dict = {}
     for user in dev_users:
-        time = local_time(user["timestamp"], user["timezone"])
+        time = get_local_time(user["timestamp"], user["timezone"])
         get_users_dict.update({user["username"]: time})
-    return get_users_dict
-
-
-def get_midnighters(users):
-    night_start = datetime.time(0, 0, 0)
-    night_end = datetime.time(6, 0, 0)
-    midnighters_dict = {}
-    for user, time in users.items():
+    night_start = 0
+    night_end = 6
+    midnighters_list = []
+    for user, time in get_users_dict.items():
         if night_start <= time < night_end:
-            midnighters_dict.update({user: time})
-    return midnighters_dict
+            midnighters_list.append(user)
+    return midnighters_list
 
 
 def print_midnighters(midnighters):
     print("Совы: ")
-    for user, time in midnighters.items():
-        print(user, time)
+    for user in midnighters:
+        print(user)
 
 
 if __name__ == "__main__":
     dev_users = load_data()
-    users = get_users(dev_users)
-    midnighters = get_midnighters(users)
+    midnighters = get_midnighters(dev_users)
     print_midnighters(midnighters)
